08f2b23ae528e93406d6bc6804bc6abef0ea578d
Mike Morano <mmorano@mikeandwan.us>
2018-10-07 11:19:59 -0400

    use a pool to manage multiprocessing, and prepare to start clustering results

--- extract_faces.py
+++ extract_faces.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from multiprocessing import Pool
 import glob
 import face_recognition
 import multiprocessing
@@ -6,14 +7,19 @@ import os
 import uuid
 
 
-image_source_root_dir = '/srv/www/website_assets/images'
-result_dir = '/home/mmorano/face_recognition'
-processes = []
-image_list = glob.glob(image_source_root_dir + '/*/*/md/*.jpg')
+# references
+# https://medium.com/@ageitgey/machine-learning-is-fun-part-4-modern-face-recognition-with-deep-learning-c3cffc121d78
+# https://github.com/ageitgey/face_recognition
+# https://www.pyimagesearch.com/2018/07/09/face-clustering-with-python/
+
+IMAGE_SOURCE_ROOT_DIR = '/srv/www/website_assets/images'
+RESULT_DIR = '/home/mmorano/face_recognition'
+
+data = []
 
 
 def save_faces(path, image, face_locations):
-    for i, face_location in enumerate(face_locations):
+    for face_location in face_locations:
         top, right, bottom, left = face_location
 
         # original code would preserve relative directory structures
@@ -22,7 +28,7 @@ def save_faces(path, image, face_locations):
 
         # new code writes to a single directory w/ unique names, to simplify
         # later clustering code
-        dest_path = os.path.join(result_dir, str(uuid.uuid4()) + ".jpg")
+        dest_path = os.path.join(RESULT_DIR, str(uuid.uuid4()) + ".jpg")
 
         face_dir = os.path.dirname(dest_path)
 
@@ -42,14 +48,24 @@ def find_faces(path):
     # This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
     # See also: find_faces_in_picture_cnn.py
     face_locations = face_recognition.face_locations(image)
+    face_encodings = face_recognition.face_encodings(image, face_locations)
 
     save_faces(path, image, face_locations)
 
+    #if len(face_locations) > 0:
+    #    print(path)
+    #    d = [{"image_path": path, "loc": loc, "encoding": enc}
+    #        for (loc, enc) in zip(face_locations, face_encodings)]
+    #    data.extend(d)
 
-for img in image_list:
-    p = multiprocessing.Process(target = find_faces, args = (img, ))
-    processes.append(p)
-    p.start()
 
-for proc in processes:
-    proc.join()
+def main():
+    print('getting list of files...')
+    image_list = glob.glob(IMAGE_SOURCE_ROOT_DIR + '/*/*/md/*.jpg')
+
+    print('processing files in parallel...')
+    pool = Pool()
+    pool.map(find_faces, image_list)
+
+
+main()
